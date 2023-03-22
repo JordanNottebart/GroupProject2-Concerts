69cb2dbe5a9243d3acb305e5c863820cb2b26927
miichielduy <michielduyvejonck001@gmail.com>
2023-03-22 14:07:12 +0100

    2 more services

---
+++ CENV_JMH.Services/ShowingDetailService.cs
@@ -0,0 +1,52 @@
+﻿using CENV_JMH.DA;
+using CENV_JMH.DO;
+
+namespace CENV_JMH.Services
+{
+    public class ShowingDetailService
+    {
+        public List<ShowingDetail> GetShowingDetails()
+        {
+            using (var repo = new Repository())
+            {
+                return repo.Details.ToList();
+            }
+        }
+
+        public ShowingDetail GetShowingDetailById(int id)
+        {
+            using (var repo = new Repository())
+            {
+                return repo.Details.FirstOrDefault(h => h.ShowingID == id) ?? new ShowingDetail();
+            }
+        }
+
+        public void UpdateShowingDetail(ShowingDetail showingDetail)
+        {
+            using (var repo = new Repository())
+            {
+                repo.Details.Attach(showingDetail);
+
+                var e = repo.ChangeTracker.Entries().FirstOrDefault(c => c.Entity ==  showingDetail);
+                e.State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+
+                repo.SaveChanges();
+            }
+        }
+
+        public void DeleteShowingDetail(int id)
+        {
+            using (var repo = new Repository())
+            {
+
+                var todelete = repo.Details.FirstOrDefault(c => c.ID == id, null);
+                if (todelete != null)
+                {
+
+                    repo.Details.Remove(todelete);
+                    repo.SaveChanges();
+                }
+            }
+        }
+    }
+}
---
+++ CENV_JMH.Services/ShowingService.cs
@@ -0,0 +1,52 @@
+﻿using CENV_JMH.DA;
+using CENV_JMH.DO;
+
+namespace CENV_JMH.Services
+{
+    public class ShowingService
+    {
+        public List<Showing> GetShowings()
+        {
+            using (var repo = new Repository())
+            {
+                return repo.Showings.ToList();
+            }
+        }
+
+        public Showing GetShowingById(int id)
+        {
+            using (var repo = new Repository())
+            {
+                return repo.Showings.FirstOrDefault(h => h.ShowingID == id) ?? new Showing();
+            }
+        }
+
+        public void UpdateShowing(Showing showing)
+        {
+            using (var repo = new Repository())
+            {
+                repo.Showings.Attach(showing);
+
+                var e = repo.ChangeTracker.Entries().FirstOrDefault(c => c.Entity ==  showing);
+                e.State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+
+                repo.SaveChanges();
+            }
+        }
+
+        public void DeleteShowing(int id)
+        {
+            using (var repo = new Repository())
+            {
+
+                var todelete = repo.Showings.FirstOrDefault(c => c.ShowingID == id, null);
+                if (todelete != null)
+                {
+
+                    repo.Showings.Remove(todelete);
+                    repo.SaveChanges();
+                }
+            }
+        }
+    }
+}
