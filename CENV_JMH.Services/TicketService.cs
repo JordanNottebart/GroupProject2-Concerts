5973bca8e06fecb3bccbbe74e0fdd061a9507766
miichielduy <michielduyvejonck001@gmail.com>
2023-03-27 16:22:21 +0200

    #61 Created Service For Tickets

---
+++ CENV_JMH.Services/TicketService.cs
@@ -0,0 +1,47 @@
+﻿using CENV_JMH.DA;
+using CENV_JMH.DO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CENV_JMH.Services
+{
+    public class TicketService
+    {
+        public List<Ticket> GetTickets() 
+        {
+            using (var repo = new Repository())
+            {
+                return repo.Tickets.ToList();
+            }
+        }
+
+        public Ticket GetTicketById(int id)
+        {
+            using (var repo = new Repository())
+            {
+                return repo.Tickets.FirstOrDefault(c => c.ID == id) ?? new Ticket();
+            }
+
+        }
+
+        public List<Ticket> GetTicketsByUser(int userId)
+        {
+            using (var repo = new Repository())
+            {
+                return repo.Tickets.Where(c => c.BuyerID == userId).ToList();
+            }
+        }
+
+        public void AddTicket(Ticket ticket)
+        {
+            using (var repo = new Repository())
+            {
+                repo.Tickets.Add(ticket);
+                repo.SaveChanges();
+            }
+        }
+    }
+}
