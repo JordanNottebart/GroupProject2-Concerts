8214c8fa12a0a4db60900c0c130490f0ca61726f
miichielduy <michielduyvejonck001@gmail.com>
2023-03-27 16:29:45 +0200

    Klaar met Services #61

---
+++ CENV_JMH.Services/UserService.cs
@@ -0,0 +1,53 @@
+﻿using CENV_JMH.DA;
+using CENV_JMH.DO;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CENV_JMH.Services
+{
+    public class UserService
+    {
+        public List<User> GetUsers()
+        {
+            using (var repo = new Repository())
+            {
+                return repo.Users.ToList();
+            }
+        }
+
+        public User GetUserById(int id) 
+        {
+            using (var repo = new Repository())
+            {
+                return repo.Users.FirstOrDefault(x => x.Id == id);
+            }
+        }
+
+        public void UpdateUser(User user)
+        {
+            using (var repo = new Repository())
+            {
+                repo.Attach(user);
+                var e = repo.ChangeTracker.Entries().FirstOrDefault(c => c.Entity == user);
+                e.State = EntityState.Modified;
+
+                repo.SaveChanges();
+            }
+        }
+
+        public void DeleteUser(int id)
+        {
+            using (var repo = new Repository())
+            {
+                var user = GetUserById(id);
+                repo.Users.Remove(user);
+                repo.SaveChanges();
+            }
+        }
+    }
+}
