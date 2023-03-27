1fcaa9d80281783d18eb89382de5fa3e8219d27d
miichielduy <michielduyvejonck001@gmail.com>
2023-03-27 16:05:34 +0200

    #58 en #59 klaar met DO

---
+++ CENV_JMH.DO/User.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CENV_JMH.DO
+{
+    [Table("User")]
+    public class User
+    {
+        [Key]
+        [Column("User_ID")]
+        public int Id { get; set; }
+
+        [Column("User_Name")]
+        public string Name { get; set; }
+
+        [Column("User_Password")]
+        public string Password { get; set; }
+
+    }
+}
